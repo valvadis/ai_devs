cfb56d3448a8f1957e3e3c3b23b0064cd9782441
Jakub Książek <jakub.ksiazek@schibsted.com>
2024-11-13 08:30:34 +0100

    Add taks nr 7

---
+++ src/task/7/index.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { Buffer } from "buffer";
+import { Chat } from "../../service/chat.js";
+import { findLocationOnMap } from "../7/prompt.js";
+import {Config} from "../../service/config.js";
+
+const chat = new Chat();
+
+const result: string = await chat.send([
+    { role: "user", content: [
+        { "type": "image_url", "image_url": { "url": "data:image/jpeg;base64," +  base64_encode('first.png')} },
+        { "type": "image_url", "image_url": { "url": "data:image/jpeg;base64," +  base64_encode('second.png')} },
+        // { "type": "image_url", "image_url": { "url": "data:image/jpeg;base64," +  base64_encode('third.png')} },
+        { "type": "image_url", "image_url": { "url": "data:image/jpeg;base64," +  base64_encode('fourth.png')} },
+    ]},
+    { role: "user", content: findLocationOnMap },
+]);
+
+console.log(result);
+
+function base64_encode(file: string): string {
+    const pathToMaps = Config.getDirname() + "/../../data/maps/"
+    const bitmap: Buffer = fs.readFileSync(pathToMaps + file);
+
+    return bitmap.toString('base64');
+}
---
+++ src/task/7/prompt.ts
@@ -0,0 +1,5 @@
+export const findLocationOnMap = `
+    Na podstawie dostarczonych fragmentów mapy określ, z jakiego miasta one pochodzą.
+    
+    Wiem, że to miasto gdzie są spichlerze i twierdza.
+`;
